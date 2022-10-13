fe540d461e8eb4207db0e420fd4240887d1e583a
Ivanna Pokhyl <ivanna.pokhyl@gmail.com>
2022-10-13 15:32:13 +0300

    Add Routes to Dialogs.jsx

--- src/Component/Main/Dialogs/Dialogs.jsx
+++ src/Component/Main/Dialogs/Dialogs.jsx
@@ -1,35 +1,59 @@
 import React from "react";
 import style from './dialogs.module.css';
 import {NavLink} from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 
-const DialogItem = () => {
-    return (
-        <div className={style.dialogItem}>Text message from ... </div>
-    )
-}
+let users = [
+    {id:1, name: "John", message: 'Hello, dear friend!'},
+    {id:2, name: "Kate", message: 'Hi, how are you?'},
+    {id:3, name: "Travis", message: "What's up?"},
+    {id:4, name: "Peter", message: 'Nice to meet you'},
+    {id:5, name: "Joan", message: 'Hello!'}
+];
 
-const DialogUser = (props) => {
-    let path = '/dialogs/'+ props.id;
+const routes = users.map(user => (
+    {path: '/dialogs/' + user.id, exact: true, name: user.name, message: user.message}
+));
+console.log(routes);
+
+const DialogMessage = (props) => {
     return (
-        <div className={style.dialogUser}>
-            <NavLink to={path}>{props.name}</NavLink>
+        <div className={style.dialogItem}>
+            {props.text}
         </div>
     )
 }
+
+const LinkUser = (props) => {
+    let path = '/dialogs/' + props.id;
+    return (
+       <div>
+           <NavLink to={path}>{props.name}</NavLink>
+       </div>
+    )
+}
+
+let usersLink = users.map(link => <LinkUser name={link.name} id={link.id}/>);
 
 const Dialogs = () => {
     return (
-        <div className={style.dialogs}>
-            <div className={style.dialogsLeft}>
-                <DialogUser name='John' id='01'/>
-                <DialogUser name='Kate' id='02'/>
-                <DialogUser name='Travis' id='03'/>
-                <DialogUser name='Peter' id='04'/>
+            <div className={style.dialogs}>
+                <div className={style.dialogsLeft}>
+                    {usersLink}
+                </div>
+
+                <div className={style.dialogsRight}>
+                        <Routes>
+                            {routes.map( (route, index) => (
+                                <Route key={index} path={route.path} exact={route.exact} render={() => <DialogMessage text={route.message}/>} />))}
+                        </Routes>
+                </div>
             </div>
-            <div className={style.dialogsRight}>
-                <DialogItem/>
-            </div>
-        </div>
     )
 }
 export default Dialogs;
